60db659afc0160b61fc168773d038ab934893f23
Suhailhadi <23FTT1824@student.pb.edu.bn>
2025-08-26 23:31:59 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # laravel
+# dop-as2
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,6 +54,14 @@
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-es-lg rounded-ee-lg lg:rounded-ss-lg lg:rounded-ee-none">
                     <h1 class="mb-1 font-medium">Let's get started</h1>
+                    <div class="mb-4">
+                    <a href="{{ route('home') }}" class="inline-flex items-center gap-2 font-medium" wire:navigate>
+                        <span class="flex h-9 w-9 items-center justify-center rounded-md">
+                            <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+                        </span>
+                        <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
+                    </a>
+                </div>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Laravel has an incredibly rich ecosystem. <br>We suggest starting with the following.</p>
                     <ul class="flex flex-col mb-4 lg:mb-6">
                         <li class="flex items-center gap-4 py-2 relative before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A] before:top-1/2 before:bottom-0 before:left-[0.4rem] before:absolute">
